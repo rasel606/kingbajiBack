d56280963c90f73e6db63720db527655331dd36d
rasel606 <arafathosensawon@gmail.com>
2025-03-03 22:19:42 +0600

    19th commit

--- app.js
+++ app.js
@@ -32,7 +32,16 @@ app.use(cors({
 }));
 
 
-app.options('*', cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Change * to specific domain if needed
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
+
 // app.get('/', function (req, res) {
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 //   });
