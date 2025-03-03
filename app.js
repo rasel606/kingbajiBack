eb2e12b8375b1fdec40b12dfd1ce0702ab676121
rasel606 <arafathosensawon@gmail.com>
2025-03-03 22:22:17 +0600

    20th commit

--- app.js
+++ app.js
@@ -32,13 +32,11 @@ app.use(cors({
 }));
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Change * to specific domain if needed
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
+router.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://kingbaji365.live");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     next();
 });
 
