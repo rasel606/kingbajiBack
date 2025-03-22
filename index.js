4f9758fd0127340b5ff20292dd8ef9b28e0f8222
rasel606 <arafathosensawon@gmail.com>
2025-03-22 22:42:16 +0600

    76th commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const app = require('./app'); // Import the main application
 
 
-app.listen(5000, '0.0.0.0', () => {
+app.listen(5000, '127.0.0.1', () => {
     console.log('API server listening on VPN interface');
   });
 
