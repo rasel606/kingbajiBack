6bc229915cea773f22eb49102b21d3af193151ec
rasel606 <arafathosensawon@gmail.com>
2025-03-22 10:22:23 +0600

    82th commit

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ mongoose.connect(URI,)
 
 
 app.use("/api/v1", router);
-app.use("/api/v1", function (req, res) {
+app.get("/api/v2", function (req, res) {
     console.log('api new running');
     res.json('api new running');
   })
