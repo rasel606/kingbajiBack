41bc58296315a1589b12b647229a49de2e637284
rasel606 <arafathosensawon@gmail.com>
2025-03-22 11:24:17 +0600

    85th commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const https = require('https');
-const fs = require('fs');
-const app = require('./app');
 
-const sslOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/kingbaji.live/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/kingbaji.live/fullchain.pem')
-};
+const app = require('./app'); // Import the main application
 
-https.createServer(sslOptions, app).listen(443, '0.0.0.0', () => {
-    console.log('HTTPS server running on port 443');
-});
+
+
+
+
+// const http = require('http');
+
+// const httpServer = http.createServer(app);
+
+
+// httpServer.listen(5000, () => {
+//     console.log('Server running on http://localhost:5000');
+// });
+app.listen(5000, '0.0.0.0', () => {
+    console.log('API server listening on VPN interface');
+  });
+
