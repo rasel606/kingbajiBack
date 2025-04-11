e3b888d1a58cfa9cb2374020466a7260a3e64d34
rasel606 <arafathosensawon@gmail.com>
2025-04-11 19:34:25 +0600

    Resolved merge c

--- app.js
+++ app.js
@@ -112,24 +112,34 @@ app.use(cookieParser());
 app.use(cookieHandler);
 
 
-const allowedOrigins = ['http://localhost:3000', 'https://kingbaji.live', 'https://www.fwick7ets.xyz'];
+// const allowedOrigins = ['http://localhost:3000', 'https://kingbaji.live', 'https://www.fwick7ets.xyz'];
+
 
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        
-        if (allowedOrigins.includes(origin)) {
-            callback(null, origin);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: ['http://localhost:3000', 'https://kingbaji.live', 'https://www.fwick7ets.xyz'], // Change to your frontend URL in production
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Set-Cookie']
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // Allow requests with no origin (like mobile apps or curl requests)
+//         if (!origin) return callback(null, true);
+        
+//         if (allowedOrigins.includes(origin)) {
+//             callback(null, origin);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     exposedHeaders: ['Set-Cookie']
+// }));
 
 // Enhanced proxy middleware
 app.use('/apiWallet', createProxyMiddleware({
