c5c0968ebd398c390ba4119a9a3eae5d09390e16
rasel606 <arafathosensawon@gmail.com>
2025-04-17 01:33:38 +0600

    105

--- app.js
+++ app.js
@@ -20,20 +20,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cookieHandler);
 
-app.use(cors());
+// app.use(cors());
 app.use(mongoSanitize());
-const allowedOrigins = ['http://localhost:3000', 'https://kingbaji.live'];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+
+// Only allow specific origin (localhost for development)
+
+
+const corsOptions = {
+  origin: ['http://localhost:3000/','https://kingbaji.live'],
+  credentials: true,
+  exposedHeaders: ['Content-Length', 'Content-Range'],
+};
+
+app.use(cors(corsOptions));
+
 
 // ✅ API Routes
 
