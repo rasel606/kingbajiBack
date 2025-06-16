c0d92e34936298f0757d16f118f7487b86578edb
rasel606 <arafathosensawon@gmail.com>
2025-06-16 19:13:46 +0600

    292nd commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(mongoSanitize());
 
 
 app.use(cors({
-  origin: '*',
+  origin: ["https://kingbaji.live","https://kgco.kingbaji.live"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
