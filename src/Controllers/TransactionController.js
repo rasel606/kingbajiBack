4eaffdbf5f0fea55bf81bf92945e25f7ff050072
rasel606 <arafathosensawon@gmail.com>
2025-06-17 00:06:49 +0600

    320nd commit

--- src/Controllers/TransactionController.js
+++ src/Controllers/TransactionController.js
@@ -184,7 +184,7 @@ exports.submitTransaction = async (req, res) => {
                 }
 
                 bonusId = depositBonus._id;
-                turnoverRequirement = (baseAmount + bonusAmount) * turnoverPercentage;
+                turnoverRequirement = (baseAmount + bonusAmount) * depositBonus.percentage;
             }
         }
 
