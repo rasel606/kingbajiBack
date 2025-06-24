1a5138a73e3f15e800188ab15cc5f7a4cc197acd
rasel606 <arafathosensawon@gmail.com>
2025-06-25 05:40:09 +0600

    401nd commit

--- src/MiddleWare/PromotionMiddleWare.js
+++ src/MiddleWare/PromotionMiddleWare.js
@@ -1,21 +1,21 @@
-const { User, Promotion, Game } = require('../models');
+// const { User, Promotion, Game } = require('../models');
 
-const checkPromotion = async (req, res, next) => {
-    const { userId, gameId } = req.body;
+// const checkPromotion = async (req, res, next) => {
+//     const { userId, gameId } = req.body;
 
-    const user = await User.findById(userId).populate('activePromotion');
-    if (!user) return res.status(404).json({ message: 'User not found' });
+//     const user = await User.findById(userId).populate('activePromotion');
+//     if (!user) return res.status(404).json({ message: 'User not found' });
 
-    if (!user.activePromotion) return next(); // যদি কোন প্রোমোশন না থাকে, তাহলে সব গেম খেলতে পারবে
+//     if (!user.activePromotion) return next(); // যদি কোন প্রোমোশন না থাকে, তাহলে সব গেম খেলতে পারবে
 
-    const promotion = user.activePromotion;
-    const isEligible = promotion.eligibleGames.some(game => game.equals(gameId));
+//     const promotion = user.activePromotion;
+//     const isEligible = promotion.eligibleGames.some(game => game.equals(gameId));
 
-    if (!isEligible) {
-        return res.status(403).json({ message: 'Not Enter Game' });
-    }
+//     if (!isEligible) {
+//         return res.status(403).json({ message: 'Not Enter Game' });
+//     }
 
-    next();
-};
+//     next();
+// };
 
-module.exports = { checkPromotion };
+// module.exports = { checkPromotion };
