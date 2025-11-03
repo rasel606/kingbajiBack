bb177e6ccc1cd3f759051f6884170b6d18c47869
rasel606 <arafathosensawon@gmail.com>
2025-11-04 04:59:30 +0600

    first-10

--- src/Router/Api.js
+++ src/Router/Api.js
@@ -41,7 +41,7 @@ const BankController = require('../Controllers/BankController');
 // const SubAdmin = require('../Models/SubAdminModel');
 const {auth} = require('../MiddleWare/auth');
 const { createBonus,getAllBonuses } = require('../Controllers/BonusTransactionController');
-const VipController = require('../Controllers/VipBonusesController');
+const BonusTransactionController = require('../Controllers/VipBonusesController');
 const { Console } = require('winston/lib/winston/transports');
 
 
