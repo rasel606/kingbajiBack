42436a4c100b33aa053bc86188af0aa5973525ab
rasel606 <arafathosensawon@gmail.com>
2025-11-04 21:34:17 +0600

    first-24

--- src/Config/db.js
+++ src/Config/db.js
@@ -4,8 +4,11 @@ const logger = require('../utils/logger');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://bajicrick247:bajicrick24@cluster0.jy667.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"); // ✅ Cleaned
+    
+    console.log('✅ MongoDB Connected');
     logger.info('✅ MongoDB Connected');
   } catch (err) {
+    console.error('❌ MongoDB connection failed:', err.message);
     logger.error('❌ MongoDB connection failed:', err.message);
     process.exit(1);
   }
