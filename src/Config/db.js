c4e2ddb47500a525d07dcc8b59a140f649cec3a6
rasel606 <arafathosensawon@gmail.com>
2025-11-05 16:04:30 +0600

    first-31

--- src/Config/db.js
+++ src/Config/db.js
@@ -3,7 +3,7 @@ const logger = require('../utils/logger');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://bajicrick247:bajicrick24@cluster0.jy667.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"); // ✅ Cleaned
+    await mongoose.connect("mongodb+srv://bajicrick247:bajicrick24@cluster0.jy667.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"); // ✅ Cleaned
     
     console.log('✅ MongoDB Connected');
     logger.info('✅ MongoDB Connected');
