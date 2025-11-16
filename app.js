680995469e9a35050ecd3affc89fbc7009f7a99e
rasel606 <arafathosensawon@gmail.com>
2025-11-16 10:16:04 +0600

    Initial commit - 1052

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ const allowedOrigins = [
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3000',
   'http://png71.live/login',
+  '*',
   process.env.FRONTEND_URL // For production
 ].filter(Boolean);
 
